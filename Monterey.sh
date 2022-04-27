0197a2587ecc21ab80a92b9d06084b2df5ca0981
Chao <37573798+Chao1988@users.noreply.github.com>
2022-04-27 11:50:41 +0800

    Change volume name to Package

--- Monterey.sh
+++ Monterey.sh
@@ -2,4 +2,4 @@
 diskpoint=$(diskutil list physical |head -n1 | awk -F "/"  '{print $3}' |awk '{print $1}')
 diskutil eject $diskpoint
 diskutil erasedisk APFS "Macintosh HD" $diskpoint
-/Volumes/os/Install\ macOS\ Monterey.app/Contents/Resources/startosinstall --volume /Volumes/Macintosh\ HD/ --agreetolicense
+/Volumes/Package/Install\ macOS\ Monterey.app/Contents/Resources/startosinstall --volume /Volumes/Macintosh\ HD/ --agreetolicense
