e059d77a026439037d04b45903e6cc76cfa59291
Chao <37573798+Chao1988@users.noreply.github.com>
2022-05-12 12:03:04 +0800

    Delete BigSur.sh

--- BigSur.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-diskpoint=$(diskutil list physical |head -n1 | awk -F "/"  '{print $3}' |awk '{print $1}')
-diskutil eject $diskpoint
-diskutil erasedisk APFS "Macintosh HD" $diskpoint
-/Volumes/Packages/Install\ macOS\ Big\ Sur.app/Contents/Resources/startosinstall --volume /Volumes/Macintosh\ HD/ --agreetolicense
